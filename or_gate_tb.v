bc3f8ddb5751918f60b32090de37384716f6dbfb
Maanash-afk <maanashappari@gmail.com>
2026-08-14 21:37:20 +0530

    or_gate
    Uploaded Verilog design, testbench, README, and simulation screenshots.

---
+++ or_gate_tb.v
@@ -0,0 +1,21 @@
+module or_gate_tb;
+reg a;
+reg b;
+wire y;
+or_gate dut(
+.a(a),
+.b(b),
+.y(y)
+);
+initial begin
+a=0;b=0;
+#10;
+a=0;b=1;
+#10;
+a=1;b=0;
+#10;
+a=1;b=1;
+#10;
+$finish();
+end
+endmodule
---
+++ orgate.v
@@ -0,0 +1,12 @@
+module orgate(
+input a,b,
+output reg y
+);
+always @(*)
+begin
+if(a||b)
+y=1;
+else
+y=0;
+end
+endmodule
