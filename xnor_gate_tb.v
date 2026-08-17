4ed1a55693f52c98b0f50d833fddbb808fe51c15
Maanash-afk <maanashappari@gmail.com>
2026-08-18 00:45:52 +0530

    xnor_gate
    uploaded verilog code , design ,test bench , waveform

---
+++ xnor_gate_tb.v
@@ -0,0 +1,28 @@
+module xnor_gate_tb;
+
+reg a,b;
+wire y;
+
+xnor_gate dut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
