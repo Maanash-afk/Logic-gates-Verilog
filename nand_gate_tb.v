e026e2e5acd171d839a267aa066eeb1d7a55df97
Maanash-afk <maanashappari@gmail.com>
2026-08-17 23:57:34 +0530

    nand_gate
    uploaded verilog code , design , testbench , waveform

---
+++ nand_gate_tb.v
@@ -0,0 +1,20 @@
+module nand_gate_tb;
+reg a;
+reg b;
+wire y;
+nand_gate DUT(
+.a(a),.b(b),
+.y(y)
+);
+initial begin
+$monitor("a=%b,b=%b,y=%b",a,b,y);
+a=0;b=0;
+#10;
+a=0;b=1;
+#10;
+a=1;b=0;
+#10;
+a=1;b=1;
+#10;
+end
+endmodule
