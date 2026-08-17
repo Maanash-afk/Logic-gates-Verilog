0c95779f9949e90e38cbdc54b7e46256c96432b3
Maanash-afk <maanashappari@gmail.com>
2026-08-18 00:11:25 +0530

    nor_gate
    uploaded verilog code , design , test bench , waveform

---
+++ nor_gate_tb.v
@@ -0,0 +1,19 @@
+module nor_gate_tb;
+reg a;reg b;
+wire y;
+nor_gate dut(
+.a(a),.b(b),
+.y(y)
+);
+initial begin
+$monitor("a=%b,b=%b,y=%b",a,b,y);
+a=0;b=0;
+#10;
+a=1;b=0;
+#10;
+a=0;b=1;
+#10;
+a=1;b=1;
+#10;
+end
+endmodule
